d82a8ac8664917bfa37adf4c6e9c2104ae09b67b
Carlos Eduardo Penido <kadu@syncsoftwares.com.br>
2017-12-29 18:05:27 -0200

    getUsersByLocationMedia

--- src/location/location-controller.js
+++ src/location/location-controller.js
@@ -1,11 +1,12 @@
 var Client = require('instagram-private-api').V1;
+var genderService = require("../gender/gender-service");
 
 exports.getLocations = async (req, res, next) => {
 
     try {
 
         const session = req.session;
-        const accountId = await session.getAccountId();
+        const accountId = req.session;
 
         Client.Location.search(session, req.body.locationName).then((loc) => {
 
@@ -20,18 +21,96 @@ exports.getLocations = async (req, res, next) => {
                 });
             }
 
-            res.status(200).send(data);
+            res.status(500).send({
+                success: true,
+                data: data
+            });
 
-        }, (err) => {
-            console.error(error);
-            res.status(500).send(error.messenge);
+        }, (e) => {
+            res.status(500).send({
+                success: false,
+                message: e.message || e
+            });
         });
+    } catch (e) {
+        res.status(500).send({
+            success: false,
+            message: e.message || e
+        });
+    }
+}
 
+exports.getUsersByLocationMedia = async (req, res, next) => {
+    try {
 
+        const session = req.session;
+        const locationId = req.body.locationId;
+        const cursor = req.body.cursor;
+        const gender = req.body.gender;
 
-    } catch (error) {
-        console.error(error);
-        res.status(500).send(error.messenge);
+        const feed = new Client.Feed.LocationMedia(session, locationId, 2000);
+
+        if (cursor) {
+            feed.setCursor(cursor);
+        }
+
+        feed.get().then((medias) => {
+
+            let index = 0;
+            let data = [];
+
+            addUserId(res, medias, index, data, feed.getCursor(), gender);
+
+        }, (e) => {
+            res.status(500).send({
+                success: false,
+                message: e.message || e
+            });
+        });
+    } catch (e) {
+        res.status(500).send({
+            success: false,
+            message: e.message || e
+        });
+    }
+}
+
+function addUserId(res, medias, index, data, cursor, gender) {
+
+    if (index >= medias.length) {
+        res.status(200).send({
+            success: true,
+            cursor: cursor,
+            data: data
+        });
+        return;
     }
 
+    const account = medias[index].params.account;
+
+    if (data.includes(account.id)) {
+        index++;
+        addUserId(res, medias, index, data, cursor, gender);
+    }
+
+    if (gender) {
+        genderService.getGender(account.fullName || account.username).then((resGender) => {
+            if (gender == resGender) {
+                data.push(account.id);
+            }
+
+            index++;
+            addUserId(res, medias, index, data, cursor, gender);
+
+        }, (e) => {
+            index++;
+            addUserId(res, medias, index, data, cursor, gender);
+        });
+
+        return;
+    }
+
+    data.push(account.id);
+    index++;
+    addUserId(res, medias, index, data, cursor, gender);
 }
--- src/location/location-router.js
+++ src/location/location-router.js
@@ -4,5 +4,6 @@ const locationController = require('./location-controller');
 const authController = require('../auth/auth-controller');
 
 router.post('/getLocations', authController.authorize, locationController.getLocations);
+router.post('/getUsersByLocationMedia', authController.authorize, locationController.getUsersByLocationMedia);
 
 module.exports = router;
